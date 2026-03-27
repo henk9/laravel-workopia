8f25a29246c6c5ed5092366cf45ba808fee4932c
henke <19148118+henk9@users.noreply.github.com>
2026-03-27 03:55:22 +0100

    Workopia. Lecture 036 added. Tailwind CSS version 4 and hot reloading now works.

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,6 +6,13 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   @vite('resources/css/app.css')
   <title>{{$title ?? 'Workopia | Find or List a Job'}}</title>
+  <link
+  rel="stylesheet"
+  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
+  integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg=="
+  crossorigin="anonymous"
+  referrerpolicy="no-referrer"
+/>
 </head>
 
 <body class="bg-gray-100">
